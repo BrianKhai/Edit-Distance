ac8118f41f1dc70f5aecf7334e6a9441b92bb377
Brian <briankhai@gmail.com>
2019-11-19 21:07:00 -0800

    Fixed alignment p1

--- Source.cpp
+++ Source.cpp
@@ -7,11 +7,11 @@ using namespace std;
 
 vector<vector<int>> calculate(vector<vector<int>> table, string str1, string str2);
 void print_table(vector<vector<int>> table);
-string* find_alignment(string* arr);
+string* find_alignment(string* arr, vector<vector<int>> table);
 
 int main() {
-	string str1 = "0asdf0";
-	string str2 = "a99s99d99f";
+	string str1 = "088128367890";
+	string str2 = "9990126790";
 	/*cout << "Enter first string\n";
 	cin >> str1;
 	cout << "Enter second string\n";
@@ -39,8 +39,8 @@ int main() {
 	cout << "The edit distance is: " << distance << endl << endl;
 
 	string arr[] = { str1, str2 };
-	string *alignment = find_alignment(arr);
-	
+	string *alignment = find_alignment(arr, table);
+
 
 	cout << "Alignment is:" << endl;
 	cout << alignment[0] << endl;
@@ -51,7 +51,7 @@ int main() {
 vector<vector<int>> calculate(vector<vector<int>> table, string str1, string str2) {
 	int len1 = str1.length() + 1;
 	int len2 = str2.length() + 1;
-	
+
 
 	for (int i = 0; i < len1; i++) {
 		table[i][0] = i;
@@ -72,7 +72,7 @@ vector<vector<int>> calculate(vector<vector<int>> table, string str1, string str
 				table[i][j] = upper_left;
 			} else if (upper_left < left && upper_left < upper) {
 				table[i][j] = upper_left + 1;
-			} else if (upper_left < left) {
+			} else if (upper < left) {
 				table[i][j] = upper + 1;
 			} else {
 				table[i][j] = left + 1;
@@ -114,33 +114,18 @@ void print_table(vector<vector<int>> table) {
 	cout << endl;
 }
 
-string* find_alignment(string* arr) {
-	string longer;
-	string shorter;
+string* find_alignment(string* arr, vector<vector<int>> table) {
+	string str1 = "";
+	string str2 = "";
+	int len1 = arr[0].length() - 1;
+	int len2 = arr[1].length() - 1;
 
-	if (arr[0].length() > arr[1].length()) {
-		longer = arr[0];
-		shorter = arr[1];
-		arr[1] = arr[0];
-	} else {
-		longer = arr[1];
-		shorter = arr[0];
-		arr[0] = arr[1];
+	for (int i = 5; i >= 0; i--) {
+
 	}
-	
-	int longer_len = longer.length() - 1;
-	int shorter_len = shorter.length() - 1;
 
-	cout << arr[0][0] << endl;
-	cout << arr[0][1] << endl;
-	cout << arr[0][10] << endl;
-	for (int i = shorter_len; i >= 0; i--) {
-		for (int j = longer_len; j >= 0; j--) {
-			if (shorter[i] == longer[j]) {
-
-			}
-		}
-	}
+	cout << str1 << endl;
+	cout << str2 << endl;
 
 	return arr;
 }
